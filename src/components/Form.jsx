aef6aaea479d1dca683b57a75ef3848d841d64cb
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-14 16:28:10 +0200

    [UPD] STRUTTURA FORM CREATA

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,8 +4,36 @@ import axios from 'axios';
 
 const Form = () => {
     return (
-        <div>Form</div>
-    )
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12'>
+                    <h1>FORM</h1>
+                </div>
+                <div className='col-12'>
+                    <form action="" className='border rounded bg-info-subtle'>
+                        <div className='row'>
+                            <div className='col-12'>
+                                <label htmlFor="" className='control-label'>Nome</label>
+                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                            </div>
+                            <div className='col-12'>
+                                <label htmlFor="" className='control-label'>Nome</label>
+                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                            </div>
+                            <div className='col-12'>
+                                <label htmlFor="" className='control-label'>Nome</label>
+                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                            </div>
+                            <div className='col-12'>
+                                <label htmlFor="" className='control-label'>Nome</label>
+                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Form;
