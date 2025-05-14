ec665c7fb72ea1ca1bb17972d2acb4edf131b525
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-14 17:06:10 +0200

    [UPD] FORM MODIFICATA

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,21 +13,24 @@ const Form = () => {
                     <form action="" className='border rounded bg-info-subtle'>
                         <div className='row'>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>Nome</label>
-                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                                <label htmlFor="" className='control-label'>author</label>
+                                <input type="text" name="author" className='form-control' placeholder='author' />
                             </div>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>Nome</label>
-                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                                <label htmlFor="" className='control-label'>title</label>
+                                <input type="text" name="title" className='form-control' placeholder='title' />
                             </div>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>Nome</label>
-                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                                <label htmlFor="" className='form-label'>Body</label>
+                                <textarea name="body" id="body" className='form-control'></textarea>
                             </div>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>Nome</label>
-                                <input type="text" name="name" className='form-control' placeholder='nome' />
+                                <label htmlFor="" className='form-check-label'>public</label>
+                                <div className='form-check'>
+                                    <input type="checkbox" name='public' id='public' className='form-check-input' />
+                                </div>
                             </div>
+
                         </div>
                     </form>
                 </div>
