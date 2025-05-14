30ee429dc313c172b2603ee7aea55a98582dec30
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-14 17:17:46 +0200

    [UPD] AGGIUNTA LA VARIABILE DI STATO SOTTO FORMA DI OGGETTO

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,21 +3,24 @@ import { useState } from 'react';
 import axios from 'axios';
 
 const Form = () => {
+    // DICHIARO LA VARIABILE DI STATO SOTTO FORMA DI OGGETTO 
+    const [formData, setFormData] = useState({});
+
     return (
-        <div className='container'>
+        <div className='container mt-4'>
             <div className='row'>
                 <div className='col-12'>
                     <h1>FORM</h1>
                 </div>
                 <div className='col-12'>
-                    <form action="" className='border rounded bg-info-subtle'>
-                        <div className='row'>
+                    <form action="" className='border rounded bg-info-subtle p-3'>
+                        <div className='row g-2'>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>author</label>
+                                <label htmlFor="" className='control-label'>Author</label>
                                 <input type="text" name="author" className='form-control' placeholder='author' />
                             </div>
                             <div className='col-12'>
-                                <label htmlFor="" className='control-label'>title</label>
+                                <label htmlFor="" className='control-label'>Title</label>
                                 <input type="text" name="title" className='form-control' placeholder='title' />
                             </div>
                             <div className='col-12'>
@@ -25,10 +28,13 @@ const Form = () => {
                                 <textarea name="body" id="body" className='form-control'></textarea>
                             </div>
                             <div className='col-12'>
-                                <label htmlFor="" className='form-check-label'>public</label>
+                                <label htmlFor="" className='form-check-label'>Public</label>
                                 <div className='form-check'>
                                     <input type="checkbox" name='public' id='public' className='form-check-input' />
                                 </div>
+                            </div>
+                            <div className='col-12'>
+                                <button type='submit' className='btn btn-primary'>Invia</button>
                             </div>
 
                         </div>
